d7a54efa4498c1ebf3f36959087247dc1cc392e4
Muthu <84056393+mkandanmk7@users.noreply.github.com>
2021-07-23 09:40:20 +0530

    Create JsonFormat.js

---
+++ JsonFormat.js
@@ -0,0 +1,12 @@
+    let resume={ 
+        "firstName":"muthu",
+        "lastName":"manikandan",
+        "age":25,
+        "intresting":["c","python","javaScript"],
+        "email":"mkandanmk7@gmail.com",
+        "area":{"native":"tirunelveli","living":"pollachi"},
+        
+       
+    };
+    console.log(resume);
+    
